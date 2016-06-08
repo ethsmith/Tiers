b144ba116a57401070cb1bdb9f4fe16bce16bcf8
TekkitCommando <tekkitcommando@gmail.com>
2016-06-08 18:40:42 -0500

    Chance command structure.

--- src/com/oakcentralnetwork/tiers/Tiers.java
+++ src/com/oakcentralnetwork/tiers/Tiers.java
@@ -19,18 +19,18 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Tiers extends JavaPlugin implements Listener  {
-	
+public class Tiers extends JavaPlugin implements Listener {
+
 	private ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-	
+
 	public void onEnable() {
-		
+
 		getServer().getPluginManager().registerEvents(this, this);
 		try {
 			saveConfig();
 			setupConfig(getConfig());
 			saveConfig();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		Bukkit.getServer().getLogger().info("[Tiers] Enabled!");
@@ -39,25 +39,21 @@ public class Tiers extends JavaPlugin implements Listener  {
 	public void onDisable() {
 		Bukkit.getServer().getLogger().info("[Tiers] Disabled!");
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args) {
-		if(!(sender instanceof Player)) {
-			return true;
-		} else {
-			Player player = (Player) sender;
-			if(cmd.getName().equalsIgnoreCase("tiers")) {
-				openGUI(player);
-			} else if(cmd.getName().equalsIgnoreCase("tier")) {
-				openGUI(player);
-			}
+		Player player = (Player) sender;
+		if (cmd.getName().equalsIgnoreCase("tiers")) {
+			openGUI(player);
+		} else if (cmd.getName().equalsIgnoreCase("tier")) {
+			openGUI(player);
 		}
 		return true;
 	}
-	
+
 	public void openGUI(Player player) {
 		Inventory inv = Bukkit.createInventory(null, 28, ChatColor.BLUE + "Tiers");
-		
+
 		ItemStack tier1 = new ItemStack(Material.WOOD_SWORD);
 		ItemMeta tier1Meta = tier1.getItemMeta();
 		ItemStack tier2 = new ItemStack(Material.STONE_SWORD);
@@ -68,7 +64,7 @@ public class Tiers extends JavaPlugin implements Listener  {
 		ItemMeta tier4Meta = tier4.getItemMeta();
 		ItemStack tier5 = new ItemStack(Material.DIAMOND_SWORD);
 		ItemMeta tier5Meta = tier5.getItemMeta();
-		
+
 		tier1Meta.setDisplayName(ChatColor.BLUE + "Tier 1");
 		tier1.setItemMeta(tier1Meta);
 		tier2Meta.setDisplayName(ChatColor.BLUE + "Tier 2");
@@ -79,36 +75,36 @@ public class Tiers extends JavaPlugin implements Listener  {
 		tier4.setItemMeta(tier1Meta);
 		tier5Meta.setDisplayName(ChatColor.BLUE + "Tier 5");
 		tier5.setItemMeta(tier1Meta);
-		
+
 		inv.setItem(11, tier1);
 		inv.setItem(12, tier2);
 		inv.setItem(13, tier3);
 		inv.setItem(14, tier4);
 		inv.setItem(15, tier5);
-		
+
 		player.openInventory(inv);
 	}
-	
+
 	@EventHandler
 	public void onInventoryClick(InventoryClickEvent event) {
-		if(!ChatColor.stripColor(event.getInventory().getName()).equalsIgnoreCase("Tiers")) {
+		if (!ChatColor.stripColor(event.getInventory().getName()).equalsIgnoreCase("Tiers")) {
 			return;
 		}
-		
+
 		Player player = (Player) event.getWhoClicked();
 		event.setCancelled(true);
-		
-		if(event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR || 
-				!event.getCurrentItem().hasItemMeta()) {
+
+		if (event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR
+				|| !event.getCurrentItem().hasItemMeta()) {
 			player.closeInventory();
 			return;
 		}
-		
-		switch(event.getCurrentItem().getType()) {
+
+		switch (event.getCurrentItem().getType()) {
 		case WOOD_SWORD:
 			String[] commands = getConfig().getString("Tier1.Commands").split(",");
-			for(String command : commands) {
-				if(command != null) {
+			for (String command : commands) {
+				if (command != null) {
 					Bukkit.dispatchCommand(console, command);
 				}
 			}
@@ -116,8 +112,8 @@ public class Tiers extends JavaPlugin implements Listener  {
 			break;
 		case STONE_SWORD:
 			String[] commands2 = getConfig().getString("Tier2.Commands").split(",");
-			for(String command : commands2) {
-				if(command != null) {
+			for (String command : commands2) {
+				if (command != null) {
 					Bukkit.dispatchCommand(console, command);
 				}
 			}
@@ -125,8 +121,8 @@ public class Tiers extends JavaPlugin implements Listener  {
 			break;
 		case GOLD_SWORD:
 			String[] commands3 = getConfig().getString("Tier3.Commands").split(",");
-			for(String command : commands3) {
-				if(command != null) {
+			for (String command : commands3) {
+				if (command != null) {
 					Bukkit.dispatchCommand(console, command);
 				}
 			}
@@ -134,8 +130,8 @@ public class Tiers extends JavaPlugin implements Listener  {
 			break;
 		case IRON_SWORD:
 			String[] commands4 = getConfig().getString("Tier4.Commands").split(",");
-			for(String command : commands4) {
-				if(command != null) {
+			for (String command : commands4) {
+				if (command != null) {
 					Bukkit.dispatchCommand(console, command);
 				}
 			}
@@ -143,8 +139,8 @@ public class Tiers extends JavaPlugin implements Listener  {
 			break;
 		case DIAMOND_SWORD:
 			String[] commands5 = getConfig().getString("Tier5.Commands").split(",");
-			for(String command : commands5) {
-				if(command != null) {
+			for (String command : commands5) {
+				if (command != null) {
 					Bukkit.dispatchCommand(console, command);
 				}
 			}
@@ -155,41 +151,42 @@ public class Tiers extends JavaPlugin implements Listener  {
 			break;
 		}
 	}
-	
+
 	private void setupConfig(FileConfiguration config) throws IOException {
-		if(!new File(getDataFolder(), "RESET.FILE").exists()) {
+		if (!new File(getDataFolder(), "RESET.FILE").exists()) {
 			new File(getDataFolder(), "RESET.FILE").createNewFile();
-			
+
 			config.set("Tier1.Commands", "broadcast hi,broadcast hello");
 			config.set("Tier2.Commands", "broadcast whats up,broadcast ethan was here");
 			config.set("Tier3.Commands", "broadcast how are you,broadcast im doing good");
 			config.set("Tier4.Commands", "broadcast hows life,broadcast meh");
 			config.set("Tier5.Commands", "broadcast i like bukkit,broadcast me too");
-			
+
 			new File(getDataFolder(), "RESET.FILE").createNewFile();
 		}
 	}
-	
+
 	/**
 	 * This commented out code is just in case it's requested.
 	 */
-	
-//	@EventHandler
-//	public void onPlayerJoin(PlayerJoinEvent event) {
-//		event.getPlayer().getInventory().addItem(new ItemStack(Material.COMPASS));
-//	}
-//	
-//	@EventHandler
-//	public void onPlayerInteract(PlayerInteractEvent event) {
-//		Action a = event.getAction();
-//		ItemStack is = event.getItem();
-//		
-//		if(a == Action.PHYSICAL || is == null || is.getType() == Material.AIR) {
-//			return;
-//		}
-//		
-//		if(is.getType() == Material.COMPASS) {
-//			openGUI(event.getPlayer());
-//		}
-//	}
+
+	// @EventHandler
+	// public void onPlayerJoin(PlayerJoinEvent event) {
+	// event.getPlayer().getInventory().addItem(new
+	// ItemStack(Material.COMPASS));
+	// }
+	//
+	// @EventHandler
+	// public void onPlayerInteract(PlayerInteractEvent event) {
+	// Action a = event.getAction();
+	// ItemStack is = event.getItem();
+	//
+	// if(a == Action.PHYSICAL || is == null || is.getType() == Material.AIR) {
+	// return;
+	// }
+	//
+	// if(is.getType() == Material.COMPASS) {
+	// openGUI(event.getPlayer());
+	// }
+	// }
 }
